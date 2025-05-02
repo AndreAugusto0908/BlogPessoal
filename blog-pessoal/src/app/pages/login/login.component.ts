4e772c28746d5bbd01222c2c7a02e623152e4828
Andre <andreaugustosilvacarvalho@gmail.com>
2025-05-02 01:12:44 -0300

    Feat: Add Login Layout Style

--- blog-pessoal/src/app/pages/login/login.component.ts
+++ blog-pessoal/src/app/pages/login/login.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { DefaultLoginComponent } from '../../components/default-login/default-login.component';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   imports: [
-    DefaultLoginComponent
+    DefaultLoginComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+    loginform!: FormGroup
+
+    constructor(){
+      this.loginform = new FormGroup({
+        usuario: new FormControl("", [Validators.required]),
+        senha: new FormControl("", [Validators.required, Validators.min(5)])
+      })
+    }
 }
