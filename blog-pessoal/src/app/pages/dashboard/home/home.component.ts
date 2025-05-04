2128a33ef86f0351bf0804ec480babadb5e93a49
Andre <andreaugustosilvacarvalho@gmail.com>
2025-05-04 03:07:58 -0300

    Feat: Iniciando Feed

--- blog-pessoal/src/app/pages/dashboard/home/home.component.ts
+++ blog-pessoal/src/app/pages/dashboard/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../../components/dashboard/navbar/navbar.component";
 import { SidebarComponent } from "../../../components/dashboard/sidebar/sidebar.component";
+import { FeedComponent } from '../../../components/dashboard/feed/feed.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, SidebarComponent],
+  imports: [
+    NavbarComponent, 
+    SidebarComponent,
+    FeedComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
