fee0e14f74978cfd836e02aaf451ad4c23222f3c
Andre <andreaugustosilvacarvalho@gmail.com>
2025-05-05 15:05:01 -0300

    Fix: Ajuste CORS

--- backblogpessoal/src/main/java/com/example/backblogpessoal/infra/config/WebConfig.java
+++ backblogpessoal/src/main/java/com/example/backblogpessoal/infra/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://blog-pessoal-4845d.web.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
